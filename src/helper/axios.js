74ac651054b5abd12be3fb1c23174b1e83551573
Abdur Rahman <a.rahman4124@gmail.com>
2021-12-30 01:07:57 +0600

    added

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import { axiosApi } from './urlConfig';
 
-const token=document.cookie
+// const token=document.cookie
+const token=localStorage.getItem('token')
+console.log(token);
 const axiosInstance = axios.create({
   baseURL: axiosApi,
   timeout: 60000,
   headers: {
+    'X-Custom-Header': 'foobar',
     Authorization: token||'',
   },
   withCredentials: true
